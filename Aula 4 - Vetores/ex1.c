f706975a1bc31fbe79cb75991c1585173f7b374d
root <root@DESKTOP-KC2MMV8.localdomain>
2026-02-23 23:25:07 -0300

    Subindo mais um exercício

---
+++ Aula 4 - Vetores/ex1.c
@@ -0,0 +1,43 @@
+/*
+Faça um programa em C que armazene 15 números inteiros em um vetor e depois
+permita que o usuário digite um número inteiro para ser buscado no vetor, se
+for encontrado o programa deve imprimir a posição desse número no vetor, caso
+contrário, deve imprimir a mensagem: "Nao encontrado!".
+*/
+
+#include <stdio.h>
+
+int main() {
+	int lista[15], buscar;
+
+	for(int i = 0; i < 15; i++) {
+		printf("Insira um numero: ");
+		scanf("%d", &lista[i]);
+	}
+
+
+	do {
+		printf("Busque por um numero na lista (0 para sair): ");
+		scanf("%d", &buscar);
+
+		if(buscar == 0) break;
+		int encontrado = 0;
+
+		for(int i = 0; i < 15; i++) {
+			if(lista[i] == buscar) {
+				printf("Numero encontrado na posição %d\n", i);
+				encontrado++;
+				break;
+			}
+		}
+		if(!encontrado) {
+			printf("Numero não encontrado\n");
+		}
+	} while(buscar != 0);
+
+
+
+
+
+	return 0;
+}
